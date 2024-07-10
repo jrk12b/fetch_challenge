37ceac9570d9f1c3ea67898160bad2c288d0ad86
Justin Kurdila <justinkurdila@gmail.com>
2024-07-10 12:31:27 -0400

    running prettier and editing node.js.yml

--- cypress/e2e/fetch_challenge.cy.js
+++ cypress/e2e/fetch_challenge.cy.js
@@ -1,159 +1,151 @@
 import selectors from '../support/selectors';
 
 describe('Fetch Challenge 2024', () => {
-  beforeEach(() => {
-    // Before each test, visit the page and reset the scale
-    cy.visit('/');
-    cy.resetScale();
-    console.log('Justin Kurdila is the best darn QA Automation Engineer ever');
-  });
+	beforeEach(() => {
+		// Before each test, visit the page and reset the scale
+		cy.visit('/');
+		cy.resetScale();
+		console.log('Justin Kurdila is the best darn QA Automation Engineer ever');
+	});
 
-  after(() => {
-    // After all tests, visit the page and reset the scale (resetting environment)
-    cy.visit('/');
-    cy.resetScale();
-  });
+	after(() => {
+		// After all tests, visit the page and reset the scale (resetting environment)
+		cy.visit('/');
+		cy.resetScale();
+	});
 
-  // group objects used to weigh
-  const group_1 = {
-    '0': selectors.left0_id,
-    '1': selectors.left1_id, 
-    '2': selectors.left2_id, 
-  };
+	// group objects used to weigh
+	const group_1 = {
+		0: selectors.left0_id,
+		1: selectors.left1_id,
+		2: selectors.left2_id,
+	};
 
-  const group_2 = {
-    '3': selectors.right0_id,
-    '4': selectors.right1_id, 
-    '5': selectors.right2_id, 
-  };
+	const group_2 = {
+		3: selectors.right0_id,
+		4: selectors.right1_id,
+		5: selectors.right2_id,
+	};
 
-  const group_3 = {
-    '6': selectors.right0_id,
-    '7': selectors.right1_id, 
-    '8': selectors.right2_id, 
-  };
+	const group_3 = {
+		6: selectors.right0_id,
+		7: selectors.right1_id,
+		8: selectors.right2_id,
+	};
 
-  it('Test Case #1: clicks on buttons (“Weigh”, “Reset”)', () => {
-    cy.weighScale();
-    cy.resetScale();
-  });
+	it('Test Case #1: clicks on buttons (“Weigh”, “Reset”)', () => {
+		cy.weighScale();
+		cy.resetScale();
+	});
 
-  it('Test Case #2: Getting the measurement results (field between the "bowls")', () => {
-    cy.getResult().then((result) => {
-      cy.log(`Result is ${result}`);
-    });
-  });
+	it('Test Case #2: Getting the measurement results (field between the "bowls")', () => {
+		cy.getResult().then((result) => {
+			cy.log(`Result is ${result}`);
+		});
+	});
 
-  it('Test Case #3: filling out the bowls grids with bar numbers (0 to 8)', () => {
-    cy.populateScaleWithGroups(group_1, group_2);
-  });
+	it('Test Case #3: filling out the bowls grids with bar numbers (0 to 8)', () => {
+		cy.populateScaleWithGroups(group_1, group_2);
+	});
 
-  it('Test Case #4: getting a list of weighing', () => {
-    cy.populateScaleWithGroups(group_1, group_2);
-    cy.weighScale();
-    cy.getWeighingsList().then((result) => {
-      cy.log(`Weighing List: ${result}`);
-    });
-  });
+	it('Test Case #4: getting a list of weighing', () => {
+		cy.populateScaleWithGroups(group_1, group_2);
+		cy.weighScale();
+		cy.getWeighingsList().then((result) => {
+			cy.log(`Weighing List: ${result}`);
+		});
+	});
 
-  it('Test Case #5: Clicking on the gold bar number at the bottom of the website and checking for the alert message', () => {
-    // array of bar numbers
-    const barNumbers = [ 
-      '0', '1', '2', '3', '4', '5', '6', '7', '8'
-    ];  
+	it('Test Case #5: Clicking on the gold bar number at the bottom of the website and checking for the alert message', () => {
+		// array of bar numbers
+		const barNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
 
-    // loop through each barNumber
-    barNumbers.forEach((barNumber) => {
-      cy.clickBarNumberAndValidate(barNumber)
-    });
-  });
+		// loop through each barNumber
+		barNumbers.forEach((barNumber) => {
+			cy.clickBarNumberAndValidate(barNumber);
+		});
+	});
 
-  it('Code the Algorithm', () => {
-    // Pre-reqs for algorithm:
-    // Divide 0-8 among three groups: group_1, group_2, and group_3
-    // Steps of algorithm:
-    // Step 1: Weigh the first two groups against each other (group_1 and group_2)
-    // Step 2: Analyze results of Step 1:
-      // If scale balances: The fake bar is in group_3
-      // If scale is greater than or less than: The fake bar is in the less than group
-    // Step 3: Weigh any two bars against each other from the suspected group from step 3
-      // If scale balances: the fake bar is the bar NOT weighed
-      // If scale is greater than or less than: The fake bar is in the less than
+	it('Code the Algorithm', () => {
+		// Pre-reqs for algorithm:
+		// Divide 0-8 among three groups: group_1, group_2, and group_3
+		// Steps of algorithm:
+		// Step 1: Weigh the first two groups against each other (group_1 and group_2)
+		// Step 2: Analyze results of Step 1:
+		// If scale balances: The fake bar is in group_3
+		// If scale is greater than or less than: The fake bar is in the less than group
+		// Step 3: Weigh any two bars against each other from the suspected group from step 3
+		// If scale balances: the fake bar is the bar NOT weighed
+		// If scale is greater than or less than: The fake bar is in the less than
 
-    // Step 1
-    cy.populateScaleWithGroups(group_1, group_2);
-    cy.weighScale();
-    cy.getResult().then((result) => {
-      cy.log(`Result is ${result}`);
-      // Step 2
-      if (result === '=') {
-        cy.log('Weighing result - group_1 and group2 are EQUAL');
-        cy.log('The fake bar is in group_3!');
-        cy.resetScale();
-        // Step 3
-        cy.populateScaleWithNumbers('6', '7');
-        cy.weighScale();
-        cy.getResult().then((result) => {
-          if (result === '=') {
-            cy.log('The fake bar is 8!');
-            cy.clickFakeNumberAndSucceed('8');
-          }
-          else if (result === '<') {
-            cy.log('The fake bar is 6!');
-            cy.clickFakeNumberAndSucceed('6');
-          }
-          else if (result === '>') {
-            cy.log('The fake bar is 7!');
-            cy.clickFakeNumberAndSucceed('7');
-          }
-        });
-      }
-      // Step 2
-      else if (result ==='<') {
-        cy.log('Weighings result - group_1 is LESS than group_2');
-        cy.log('The fake bar is in group_1!');
-        cy.resetScale();
-        // Step 3
-        cy.populateScaleWithNumbers('0', '1');
-        cy.weighScale();
-        cy.getResult().then((result) => {
-          if (result === '=') {
-            cy.log('The fake bar is 2!');
-            cy.clickFakeNumberAndSucceed('2');
-          }
-          else if (result ==='<') {
-            cy.log('The fake bar is 0!');
-            cy.clickFakeNumberAndSucceed('0');
-          }
-          else if (result ==='>') {
-            cy.log('The fake bar is 1!');
-            cy.clickFakeNumberAndSucceed('1');
-          }
-        });
-      }
-      // Step 2
-      else if (result ==='>') {
-        cy.log('Weighings result - group_1 is GREATER than group_2');
-        cy.log('The fake bar is in group_2!');
-        cy.resetScale();
-        // Step 3
-        cy.populateScaleWithNumbers('3', '4');
-        cy.weighScale();
-        cy.getResult().then((result) => {
-          if (result === '=') {
-            cy.log('The fake bar is 5!');
-            cy.clickFakeNumberAndSucceed('5');
-          }
-          else if (result ==='<') {
-            cy.log('The fake bar is 3!');
-            cy.clickFakeNumberAndSucceed('3');
-          }
-          else if (result ==='>') {
-            cy.log('The fake bar is 4!');
-            cy.clickFakeNumberAndSucceed('4');
-          }
-        });
-      }
-    });
-  });
+		// Step 1
+		cy.populateScaleWithGroups(group_1, group_2);
+		cy.weighScale();
+		cy.getResult().then((result) => {
+			cy.log(`Result is ${result}`);
+			// Step 2
+			if (result === '=') {
+				cy.log('Weighing result - group_1 and group2 are EQUAL');
+				cy.log('The fake bar is in group_3!');
+				cy.resetScale();
+				// Step 3
+				cy.populateScaleWithNumbers('6', '7');
+				cy.weighScale();
+				cy.getResult().then((result) => {
+					if (result === '=') {
+						cy.log('The fake bar is 8!');
+						cy.clickFakeNumberAndSucceed('8');
+					} else if (result === '<') {
+						cy.log('The fake bar is 6!');
+						cy.clickFakeNumberAndSucceed('6');
+					} else if (result === '>') {
+						cy.log('The fake bar is 7!');
+						cy.clickFakeNumberAndSucceed('7');
+					}
+				});
+			}
+			// Step 2
+			else if (result === '<') {
+				cy.log('Weighings result - group_1 is LESS than group_2');
+				cy.log('The fake bar is in group_1!');
+				cy.resetScale();
+				// Step 3
+				cy.populateScaleWithNumbers('0', '1');
+				cy.weighScale();
+				cy.getResult().then((result) => {
+					if (result === '=') {
+						cy.log('The fake bar is 2!');
+						cy.clickFakeNumberAndSucceed('2');
+					} else if (result === '<') {
+						cy.log('The fake bar is 0!');
+						cy.clickFakeNumberAndSucceed('0');
+					} else if (result === '>') {
+						cy.log('The fake bar is 1!');
+						cy.clickFakeNumberAndSucceed('1');
+					}
+				});
+			}
+			// Step 2
+			else if (result === '>') {
+				cy.log('Weighings result - group_1 is GREATER than group_2');
+				cy.log('The fake bar is in group_2!');
+				cy.resetScale();
+				// Step 3
+				cy.populateScaleWithNumbers('3', '4');
+				cy.weighScale();
+				cy.getResult().then((result) => {
+					if (result === '=') {
+						cy.log('The fake bar is 5!');
+						cy.clickFakeNumberAndSucceed('5');
+					} else if (result === '<') {
+						cy.log('The fake bar is 3!');
+						cy.clickFakeNumberAndSucceed('3');
+					} else if (result === '>') {
+						cy.log('The fake bar is 4!');
+						cy.clickFakeNumberAndSucceed('4');
+					}
+				});
+			}
+		});
+	});
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,92 +2,95 @@ import selectors from '../support/selectors';
 
 // This command clicks the reset button which removes all numbers from the scale
 Cypress.Commands.add('resetScale', () => {
-    cy.get(selectors.gameInfo_class).prev().find(selectors.reset_id).click();
+	cy.get(selectors.gameInfo_class).prev().find(selectors.reset_id).click();
 });
 
 // This command clicks the weigh button which initializes the weighing of the scale
 Cypress.Commands.add('weighScale', () => {
-    cy.get(selectors.weigh_id).click();
-    cy.wait(3000);
+	cy.get(selectors.weigh_id).click();
+	cy.wait(3000);
 });
 
 // This command gets the result of the weighing and returns the text (=, >, <)
 Cypress.Commands.add('getResult', () => {
-    cy.get(selectors.result_class).should('be.visible');
-    cy.get(selectors.result_class).contains('Result').should('be.visible');
-    cy.get(selectors.reset_id).filter(':disabled').as('disabledResultButton');
-    cy.get('@disabledResultButton').should('exist');
-    cy.get('@disabledResultButton').should('be.visible');
-    return cy.get('@disabledResultButton').invoke('text');
+	cy.get(selectors.result_class).should('be.visible');
+	cy.get(selectors.result_class).contains('Result').should('be.visible');
+	cy.get(selectors.reset_id).filter(':disabled').as('disabledResultButton');
+	cy.get('@disabledResultButton').should('exist');
+	cy.get('@disabledResultButton').should('be.visible');
+	return cy.get('@disabledResultButton').invoke('text');
 });
 
 // This command populates the scale with groups of numbers
 // This command accepts group objects as parameters. Object should include number as key and selector id as value
 // Example: group_1 = { '0': selectors.left0_id, '1': selectors.left1_id, };
 Cypress.Commands.add('populateScaleWithGroups', (group_1, group2) => {
-    cy.resetScale();
-    for (const key in group_1) {
-        const value = group_1[key];
-        cy.get(value).type(key);
-      };
-      for (const key in group2) {
-        const value = group2[key];
-        cy.get(value).type(key);
-    };
+	cy.resetScale();
+	for (const key in group_1) {
+		const value = group_1[key];
+		cy.get(value).type(key);
+	}
+	for (const key in group2) {
+		const value = group2[key];
+		cy.get(value).type(key);
+	}
 });
 
 // This command populates the scale with two specific numbers
 // This command accepts two strings to weigh
 Cypress.Commands.add('populateScaleWithNumbers', (bar1, bar2) => {
-    cy.resetScale();
-    cy.get(selectors.left0_id).type(bar1);
-    cy.get(selectors.right0_id).type(bar2);
+	cy.resetScale();
+	cy.get(selectors.left0_id).type(bar1);
+	cy.get(selectors.right0_id).type(bar2);
 });
 
 // This command gets the list of weighings and returns the list
 Cypress.Commands.add('getWeighingsList', () => {
-    cy.get(selectors.gameInfo_class).find('ol', { timeout: 3000 }).as('weighings_ol');
-    return cy.get('@weighings_ol').last().invoke('text');
+	cy.get(selectors.gameInfo_class).find('ol', { timeout: 3000 }).as('weighings_ol');
+	return cy.get('@weighings_ol').last().invoke('text');
 });
 
 // This command counts the number of weighings in the list and returns the count
 Cypress.Commands.add('countWeighings', () => {
-    cy.get('ol').find('li').its('length').then((count) => {
-        return count;
-    });
+	cy.get('ol')
+		.find('li')
+		.its('length')
+		.then((count) => {
+			return count;
+		});
 });
 
 // This command clicks the fake bar number and validates the alert is Yay! You find it!
 // This command accepts one string as a parameter
 Cypress.Commands.add('clickFakeNumberAndValidate', (barNumber) => {
-    cy.get(selectors.coins_class).contains(barNumber).click();
-    cy.on('window:alert', (alertText) => {
-        console.log(alertText);
-        expect(alertText).to.eq('Yay! You find it!');
-    });
+	cy.get(selectors.coins_class).contains(barNumber).click();
+	cy.on('window:alert', (alertText) => {
+		console.log(alertText);
+		expect(alertText).to.eq('Yay! You find it!');
+	});
 });
 
 // This command clicks the fake bar number and logs the weighing list, weiging count, and alert text
 // This command accepts one string as a parameter
 Cypress.Commands.add('clickFakeNumberAndSucceed', (barNumber) => {
-    cy.clickFakeNumberAndValidate(barNumber);
-    cy.countWeighings().as('countWeighingsResult');
-    cy.getWeighingsList().as('weighingListResult')
+	cy.clickFakeNumberAndValidate(barNumber);
+	cy.countWeighings().as('countWeighingsResult');
+	cy.getWeighingsList().as('weighingListResult');
 
-    cy.get('@countWeighingsResult').then((countWeighingsResult) => {
-        cy.log(`NUMBER OF WEIGHINGS: ${countWeighingsResult}`);
-    });
-    cy.get('@weighingListResult').then((weighingListResult) => {
-        cy.log(`COMPLETE WEIGHING LIST: ${weighingListResult}`);
-    });
+	cy.get('@countWeighingsResult').then((countWeighingsResult) => {
+		cy.log(`NUMBER OF WEIGHINGS: ${countWeighingsResult}`);
+	});
+	cy.get('@weighingListResult').then((weighingListResult) => {
+		cy.log(`COMPLETE WEIGHING LIST: ${weighingListResult}`);
+	});
 });
 
 // This command clicks on the bar number and validates the alert text is either Oops or Yay
 // This command accepts one string as a parameter
 Cypress.Commands.add('clickBarNumberAndValidate', (barNumber) => {
-    cy.get(selectors.coins_class).contains(barNumber).click();
-    cy.on('window:alert', (alertText) => {
-        console.log(alertText);
-        expect(alertText).to.be.oneOf(['Oops! Try Again!', 'Yay! You find it!']);
-    });
+	cy.get(selectors.coins_class).contains(barNumber).click();
+	cy.on('window:alert', (alertText) => {
+		console.log(alertText);
+		expect(alertText).to.be.oneOf(['Oops! Try Again!', 'Yay! You find it!']);
+	});
 });
